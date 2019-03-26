22fa4aef333d7bed9b5504f824d3bf255f587514
Nicholas Petry <claus.ironstar@outlook.com>
2019-03-26 15:35:18 +0000

    test commit from VS2017

--- PythonTestingTesting/PythonTestingTesting.py
+++ PythonTestingTesting/PythonTestingTesting.py
@@ -1,1 +1,22 @@
+print ("How many pythons?!")
+
+starting_pythons = 305000
+
+print (starting_pythons)
+
+print ("And if exactly half of your pythons each have 17 baby pythons?")
+
+baby_pythons = starting_pythons * .5 * 17
+
+print (baby_pythons)
+
+print ("Okay great. How many pythons will you have to feed and provide for now?")
+
+total_pythons = starting_pythons + baby_pythons
+
+print (total_pythons)
+
+
+
+
 
