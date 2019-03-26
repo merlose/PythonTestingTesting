a95eefb8f12e889a5bf0dfaf7a2478955908c389
Nicholas Petry <claus.ironstar@outlook.com>
2019-03-26 15:55:07 +0000

    added some subtraction and limiting a decimal to 2 places

--- PythonTestingTesting/PythonTestingTesting.py
+++ PythonTestingTesting/PythonTestingTesting.py
@@ -16,6 +16,29 @@ total_pythons = starting_pythons + baby_pythons
 
 print (total_pythons)
 
+print ("Next let's buy some extra pythons...")
+
+print ("How much money is there?!")
+
+money = 45
+
+print (money)
+
+print ("Cool. Here pythons each cost 11.55")
+
+new_python = 11.55
+
+print ("So if you buy 3 new pythons, how much money do you have left?")
+
+money_left = money - new_python * 3
+
+print ("%.2f" % money_left)
+
+print ("Neat! But not quite enough to afford the bus fare home to you house full of pythons. Best get walking.")
+
+
+
+
 
 
 
