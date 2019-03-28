e40f116a8db8d09d62e91109867ca441dd0368dc
merlose <37470789+merlose@users.noreply.github.com>
2019-03-28 19:21:27 +0000

    Add files via upload

---
+++ PythonTestingTesting.py
@@ -0,0 +1,27 @@
+#booleans
+
+#equal comparison " == "
+bool1 = 7
+bool2 = 8
+
+print ("bool1 = bool2?")
+print (bool1 == bool2)
+
+
+#not equal to " != "
+print ("is 1 not equal to 2?")
+print (1 != 2)
+
+#greater than and less than " > " and " < "
+print ("is 4 greater than 3?")
+print (4 > 3)
+
+print ("is 7 less than 5?")
+print (7 < 5)
+
+#greater than or equal to " >= " & less than or equal to " <= "
+
+print ("is 13 less than or equal to 13?")
+print (13 <= 13)
+print ("is 9 greater than or equal to 9?")
+print (9 >= 9)
